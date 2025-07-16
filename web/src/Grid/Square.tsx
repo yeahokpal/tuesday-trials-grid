877eddc9a4318d3b7f2f129b569a635de53d0f3f
Will Kennedy <willmlb@sbcglobal.net>
2025-07-16 18:30:47 -0400

    display name update

--- web/src/Grid/Square.tsx
+++ web/src/Grid/Square.tsx
@@ -3,14 +3,22 @@ import { useEffect, useState } from "react"
 import { Database } from "sql.js"
 
 function Square({select, chosen, selected, db}: {select: () => void, chosen: string | null, selected: boolean, db: Database}) {
-    const [details, setDetails] = useState<{name: string | undefined, pfp: string | undefined} | undefined>(undefined);
+    const [details, setDetails] = useState<{name: string | undefined, pfp: string | undefined, prefix: string | undefined} | undefined>(undefined);
     useEffect(() => {
-        setDetails(db.exec("SELECT Name, ProfileUrl FROM Player WHERE DisplayName = ? AND Main = 1", [chosen]).at(0)?.values.map(v => ({name: v.at(0)?.toString(), pfp: v.at(1)?.toString()})).at(0));
+        setDetails(db.exec("SELECT Name, ProfileUrl, Prefix FROM Player WHERE DisplayName = ? AND Main = 1", [chosen]).at(0)?.values.map(v => ({name: v.at(0)?.toString(), pfp: v.at(1)?.toString(), prefix: v.at(2)?.toString()})).at(0));
     }, [chosen])
-    return <div className={classNames("gridSquare", {"selected": selected, "guessed": chosen != null, "smaller": (chosen?.length ?? 0) > 10})} onClick={chosen == null? select : undefined}>
+    let displayName = chosen;
+    let subName = undefined;
+    if (details?.name && details.name !== chosen)
+        subName = details.name;
+    if ((details?.prefix?.length ?? 0) > 0)
+        if (subName) subName = details?.prefix + " | " + subName;
+        else displayName = details?.prefix + " | " + displayName;
+
+    return <div className={classNames("gridSquare", {"selected": selected, "guessed": displayName != null, "smaller": (displayName?.length ?? 0) > 10})} onClick={chosen == null? select : undefined}>
         {details?.pfp && <img src={details.pfp} />}
-        <h2 className="gridLabel" hidden={!chosen}>{chosen}</h2>
-        {details?.name && details.name !== chosen && <p className="gridSubLabel" style={{color: "lightgray"}}>({details.name})</p>}
+        <h2 className="gridLabel" hidden={!chosen}>{displayName}</h2>
+        {details?.name && details.name !== chosen && <p className="gridSubLabel" style={{color: "lightgray"}}>({subName})</p>}
     </div>
 }
 export default Square
